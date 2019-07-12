9481a87dd94c6e1764224fcbf89f628e1ac70f3a
VladSimeonidi <simeonidiv@gmail.com>
2019-07-12 10:14:36 +0300

    Переделаное экзам

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4500,
+  arrows: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+$('.lightbox').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: false,
+  infinite: false
+});
