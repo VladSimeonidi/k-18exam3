85aed416532a92ceab9228f02dd5031dcab72883
VladSimeonidi <simeonidiv@gmail.com>
2019-09-26 13:27:46 +0300

    button added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+$(document).ready(function(){
+  $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
 
-$('.slider').slick({
+  $('.slider').slick({
   dots: true,
   infinite: true,
   autoplay: true,
@@ -22,4 +24,22 @@ $('.lightbox').slick({
 $("#menuToggle").click(function() {
   $(".block_1").toggleClass('burger__dispaly');
 });
+});
 
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+$(function() {
+ $("#go-top").scrollToTop();
+});
