0c470d7438422f3f7d1b7bfd13dab9485709ec42
VladSimeonidi <simeonidiv@gmail.com>
2019-09-30 19:18:13 +0300

    mouse click added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-  $('body').append('<a href="#" id="popup__toggle" onclick="return false;"><div class="circlephone" style="transform-origin: center;"></div><div class="circle-fill" style="transform-origin: center;"></div><div class="img-circle" style="transform-origin: center;"><div class="img-circleblock" style="transform-origin: center;"></div></div></a>');
-
+$(document).ready(function() {
   $('.slider').slick({
   dots: true,
   infinite: true,
@@ -23,22 +21,39 @@ $('.lightbox').slick({
 $("#menuToggle").click(function() {
   $(".block_1").toggleClass('burger__dispaly');
 });
-});
+
+$('body').append('<a href="#" id="popup__toggle" onclick="return false;"><div class="circlephone" style="transform-origin: center;"></div><div class="circle-fill" style="transform-origin: center;"></div><div class="img-circle" style="transform-origin: center;"><div class="img-circleblock" style="transform-origin: center;"></div></div></a>');
 
 $(function() {
  $.fn.scrollToTop = function() {
   $(this).hide().removeAttr("href");
-  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow");
   var scrollDiv = $(this);
   $(window).scroll(function() {
-   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-   else $(scrollDiv).fadeIn("slow")
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow");
+   else $(scrollDiv).fadeIn("slow");
   });
   $(this).click(function() {
-   $("html, body").animate({scrollTop: 0}, "slow")
-  })
- }
+   $("html, body").animate({scrollTop: 0}, "slow");
+  });
+ };
 });
+
 $(function() {
  $("#popup__toggle").scrollToTop();
+});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const cursor = document.querySelector('.mause');
+    document.addEventListener('mousemove', function (e) {
+    cursor.setAttribute('style', 'top: '+(e.pageY -10)+'px; left: '+(e.pageX -10)+'px;');
+    console.log(e);
+});
+  document.addEventListener('click', function() {
+    cursor.classList.add('mause_click');
+    setTimeout(function () {
+      cursor.classList.remove('mause_click');
+    }, 300);
+  });
 });
